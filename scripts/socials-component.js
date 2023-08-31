fd09a9b6ad4ddecaa9dda53ec14c547c415eab4f
NickBirrer <nicolasruzonbirrer@gmail.com>
2023-08-31 20:23:42 -0300

    Arrumando o link de rigging/icones e o tamanho do menu

--- scripts/socials-component.js
+++ scripts/socials-component.js
@@ -5,11 +5,11 @@ socialsTemplate.innerHTML = `
   <link rel="stylesheet" type="text/css" href="./styles/socials-styles.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <div id="socials-component">
-    <a href="https://www.instagram.com/blue_lightfoot/" target="_blank" class="fa fa-facebook fa-lg"></a>
+    <a href="https://twitter.com/bluethatsred?t=7To91GrJQUvOgF0zbBNuiA&s=09" target="_blank" class="fa fa-twitter fa-lg"></a>
     <a href="https://www.instagram.com/blue_lightfoot/" target="_blank" class="fa fa-instagram fa-lg"></a>
-    <a href="https://www.instagram.com/blue_lightfoot/" target="_blank" class="custom-icon bluesky"></a>
-    <a href="https://www.instagram.com/blue_lightfoot/" target="_blank" class="fa fa-tumblr fa-lg"></a>
-    <a href="https://www.instagram.com/blue_lightfoot/" target="_blank" class="custom-icon artstation"></a>
+    <a href="https://bsky.app/profile/bluethatsred.bsky.social" target="_blank" class="custom-icon bluesky"></a>
+    <a href="https://www.tumblr.com/bluelightfoot?source=share" target="_blank" class="fa fa-tumblr fa-lg"></a>
+    <a href="https://www.artstation.com/barbararezei4" target="_blank" class="custom-icon artstation"></a>
   </div>
 `;
 
